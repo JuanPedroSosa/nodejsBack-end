27e7ba343d1dd206f2d7b28c261dc92339501e6c
Juan Pedro Sosa <juanpedrososa@outlook.com>
2020-05-27 21:48:14 -0300

    cambiamos el host de las websocket en producción

--- realtime/client.js
+++ realtime/client.js
@@ -1,6 +1,11 @@
 const io = require("socket.io-client");
 
-let socket = io.connect("http://localhost:8000", {reconnect: true});
+let host = "http://localhost:8000";
+// modo producción lo guarda en la tabla session
+if (process.env.NODE_ENV && process.env.NODE_ENV == "production")
+	shost = "https://tranquil-mountain-62007.herokuapp.com/";
+
+let socket = io.connect(host, {reconnect: true});
 
 socket.on("connect", function() {
 	console.log("\n\nSocket connected\n\n");
